426de6a9d62cea3f445fc1110ffeadf75efd61bd
reactjspro09 <reactjspro990@gmail.com>
2024-10-04 13:05:02 +0530

    Update cors package and make other necessary changes

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json()); // Parse JSON request bodies
 
 // Razorpay instance initialization with API Key and Secret
 const razorpay = new Razorpay({
-  key_id: 'YOUR_RAZORPAY_KEY_ID', // Replace with your Razorpay Key ID
-  key_secret: 'YOUR_RAZORPAY_KEY_SECRET' // Replace with your Razorpay Key Secret
+  key_id: 'rzp_live_jo7kU7mTObULQq', // Replace with your Razorpay Key ID
+  key_secret: 'vMi0yHNpnt79xnY3LPPGKIul' // Replace with your Razorpay Key Secret
 });
 
 // Route to create a Razorpay order
